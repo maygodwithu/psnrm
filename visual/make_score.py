1bd863730bec9fd9feab67bc502d21d9de938806
maygodwithu <maygodwithu@naver.com>
2020-01-29 16:13:06 +0900

    add score image

---
+++ visual/make_score.py
@@ -0,0 +1,46 @@
+import torch
+import seaborn as sns
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+def getRepr(fname):
+    data = []
+    fp = open(fname + ".info", 'r')
+    for line in fp:
+        data.append(line)
+
+    rep = np.fromstring(data[1].rstrip(), dtype=float, sep=' ')
+    
+    return rep[1:]
+    
+mode="all"
+
+doc_fname ="../result/epath.doc."+mode
+q_fname ="../result/epath.query."+mode
+#1. read repr doc, query
+doc_repr = getRepr(doc_fname)
+q_repr = getRepr(q_fname)
+
+#2. compute score
+dot = doc_repr * q_repr
+score = np.sum(dot)
+
+print('score = ', score)
+
+sorted_ind = np.flip(np.argsort(dot))
+k=0
+ssum=0
+for ind in sorted_ind:
+    ssum += dot[ind]
+    print(ind, '=', dot[ind], '\t', int(ssum / score * 10000)/100)
+    k += 1
+    if(k > 10): break
+
+
+#3. draw graph
+plt.figure(figsize=(10, 10))
+df = pd.DataFrame(dict(dim=np.arange(10000), value=doc_repr))
+g = sns.relplot(x='dim', y='value', data=df)
+
+plt.savefig("repr.png")
